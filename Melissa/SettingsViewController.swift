e78e68f2c58b9575abdf9a63c8fe991026372645
Daniel Farrelly <daniel@jellystyle.com>
2016-02-18 15:55:12 +1100

    Improve the method for updating the tableview

--- Melissa/SettingsViewController.swift
+++ Melissa/SettingsViewController.swift
@@ -24,127 +24,19 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 		messagesSection.headerText = "Messages"
 		self.dataSource.addSection(messagesSection)
 
-		self.updateView()
-
+		self._updateView()
 	}
 
-	func updateView() {
-
-		let preferences = PreferencesManager(suiteName: "group.com.jellystyle.Melissa")
-
-		if let section = self.dataSource.sectionWithKey("contact") {
-			section.removeAllRows();
-
-			if let contact = preferences?.contact {
-
-				let contactRow = JSMStaticRow(key: "contact")
-				if let imageData = contact.thumbnailImageData, let image = UIImage(data: imageData) {
-					let source = image.CGImage
-					let scale = UIScreen.mainScreen().scale
-
-					let size = 44 * scale
-
-					let context = CGBitmapContextCreate(nil, Int(size), Int(size), CGImageGetBitsPerComponent(source), 0, CGImageGetColorSpace(source), CGImageGetBitmapInfo(source).rawValue)
-
-					let percent = size / min(image.size.width * image.scale, image.size.height * image.scale)
-					let s = CGSize(width: image.size.width * image.scale * percent, height: image.size.height * image.scale * percent)
-					let o = CGPoint(x: ((s.width - size) / 2), y: ((s.height - size) / 2))
-					CGContextDrawImage(context, CGRect(origin: o, size: s), source)
-
-					if let imageRef = CGBitmapContextCreateImage(context) {
-						contactRow.image = UIImage(CGImage: imageRef, scale: scale, orientation: image.imageOrientation)
-					}
-				}
-				let formatter = CNContactFormatter()
-				contactRow.text = formatter.stringFromContact(contact)
-				contactRow.configurationForCell {
-					(row, cell) in
-					if let imageView = cell.imageView, let image = row.image {
-						imageView.layer.cornerRadius = image.size.width / 2
-						imageView.layer.masksToBounds = true
-					}
-				}
-				section.addRow(contactRow)
-
-				let recipientSection = JSMStaticSection(key: "contact-2")
-				self.dataSource.insertSection(recipientSection, atIndex: self.dataSource.indexForSection(section) + 1)
-
-				let callRow = JSMStaticSelectPreference.transientPreferenceWithKey("call-recipient")
-				callRow.text = "Calls"
-				callRow.value = preferences!.callRecipient
-				callRow.options = preferences!.callOptions.map({
-					(labelledValue) in
-					if let phoneNumber = labelledValue.value as? CNPhoneNumber {
-						return [JSMStaticSelectOptionLabel: phoneNumber.stringValue, JSMStaticSelectOptionValue: phoneNumber.stringValue]
-					}
-					return [JSMStaticSelectOptionLabel: labelledValue.value, JSMStaticSelectOptionValue: labelledValue.value]
-				})
-				callRow.addObserver(self)
-				recipientSection.addRow(callRow)
-
-				let messageRow = JSMStaticSelectPreference.transientPreferenceWithKey("message-recipient")
-				messageRow.text = "Messages"
-				messageRow.value = preferences!.messageRecipient
-				messageRow.options = preferences!.messageOptions.map({
-					(labelledValue) in
-					if let phoneNumber = labelledValue.value as? CNPhoneNumber {
-						return [JSMStaticSelectOptionLabel: phoneNumber.stringValue, JSMStaticSelectOptionValue: phoneNumber.stringValue]
-					}
-					return [JSMStaticSelectOptionLabel: labelledValue.value, JSMStaticSelectOptionValue: labelledValue.value]
-				})
-				messageRow.addObserver(self)
-				recipientSection.addRow(messageRow)
-			}
-
-			else {
-				let row = JSMStaticRow(key: "select-contact")
-				row.text = "Select contact…"
-				row.configurationForCell {
-					(row, cell) in
-					cell.textLabel?.textColor = UIColor(red: 0, green: 0.506, blue: 0.83, alpha: 1)
-				}
-				section.addRow(row)
-
-				self.dataSource.removeSection(self.dataSource.sectionWithKey("contact-2"))
-			}
-
-		}
-
-		if let section = self.dataSource.sectionWithKey("messages") {
-			section.removeAllRows();
-
-			if let messages = preferences?.messages {
-				for message in messages {
-
-					let row = JSMStaticTextPreference.transientPreferenceWithKey(message)
-					row.value = message
-					row.textField?.placeholder = "Message Text"
-					row.canBeMoved = true
-					row.canBeDeleted = true
-					row.fitControlToCell = true
-					row.addObserver(self)
-					section.addRow(row)
-
-				}
-			}
-
-			let row = JSMStaticRow(key: "add-message")
-			row.text = "Add Message"
-			row.canBeDeleted = true
-			row.configurationForCell {
-				(row, cell) in
-				cell.textLabel?.textColor = UIColor(red: 0, green: 0.506, blue: 0.83, alpha: 1)
-			}
-			section.addRow(row)
-
-		}
-
-	}
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
+    }
 
 	// MARK: IBActions
 
 	@IBAction func unwindToMain(segue: UIStoryboardSegue) {
-		self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+        self.view.endEditing(true)
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
 	}
 
 	// MARK: Table view delegate
@@ -152,12 +44,13 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
 		tableView.deselectRowAtIndexPath(indexPath, animated: true)
 
-		if let dataSource = tableView.dataSource as? JSMStaticDataSource {
-			let row = dataSource.rowAtIndexPath(indexPath)
+		if let dataSource = tableView.dataSource as? JSMStaticDataSource, let row = dataSource.rowAtIndexPath(indexPath) {
 			if let row = row as? JSMStaticSelectPreference {
-				self.navigationController?.pushViewController(row.viewController, animated: true)
-			}
-			else if (row.key as? String == "contact" || row.key as? String == "select-contact") {
+
+                self.navigationController?.pushViewController(row.viewController, animated: true)
+
+            }
+            else if row.key as? String == "contact" || row.key as? String == "select-contact" {
 
 				let viewController = CNContactPickerViewController()
 				viewController.delegate = self
@@ -165,17 +58,19 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 				self.presentViewController(viewController, animated: true, completion: nil)
 
 			}
-			else if (row.key as? String == "add-message") {
+            else if row.key as? String == "add-message" {
 
 				let empty = JSMStaticTextPreference(key: String(indexPath.row))
 				empty.value = nil
 				empty.textField?.placeholder = "Message Text"
+                empty.textField?.returnKeyType = .Done
 				empty.canBeMoved = true
 				empty.canBeDeleted = true
 				empty.fitControlToCell = true
 				empty.addObserver(self)
 
-				dataSource.insertRow(empty, intoSection: row.section, atIndex: UInt(indexPath.row), withRowAnimation: UITableViewRowAnimation.Bottom)
+                dataSource.insertRow(empty, intoSection: row.section, atIndex: UInt(indexPath.row), withRowAnimation: UITableViewRowAnimation.Bottom)
+                empty.textField?.becomeFirstResponder()
 
 			}
 		}
@@ -196,11 +91,8 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 	}
 
 	override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-		if let dataSource = tableView.dataSource as? JSMStaticDataSource {
-			let row = dataSource.rowAtIndexPath(indexPath)
-			if row.key as? String == "contact" {
-				return 60
-			}
+		if let dataSource = tableView.dataSource as? JSMStaticDataSource, let row = dataSource.rowAtIndexPath(indexPath) where row.key as? String == "contact" {
+            return 60
 		}
 		return 44
 	}
@@ -210,7 +102,7 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 	func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
 		if let preferences = self.preferences {
 			preferences.contact = contact
-			self.updateView()
+			self._updateView()
 			self.tableView.reloadData()
 		}
 
@@ -221,6 +113,14 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 
 	// MARK: Static data source delegate
 
+    override func dataSource(dataSource: JSMStaticDataSource!, rowNeedsReload row: JSMStaticRow!, atIndexPath indexPath: NSIndexPath!) {
+        // We don't need to reload the row, it gets reloaded when the view appears
+    }
+
+    override func dataSource(dataSource: JSMStaticDataSource!, sectionNeedsReload section: JSMStaticSection!, atIndex index: UInt) {
+        // We don't need to reload the section, it gets reloaded when the view appears
+    }
+    
 	override func dataSource(dataSource: JSMStaticDataSource!, didMoveRow row: JSMStaticRow!, fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
 		self._updateMessagesInUserDefaults()
 	}
@@ -254,7 +154,6 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 
 			}
 		}
-
 		else {
 			self._showMessage("Something went wrong while updating your preferences. Try again in a minute or three.")
 		}
@@ -262,17 +161,191 @@ class SettingsViewController: JSMStaticTableViewController, JSMStaticPreferenceO
 
 	// MARK: Utilities
 
+    private func _updateView() {
+        self._updateContactSection()
+        self._updateRecipientSection()
+        self._updateMessagesSection()
+    }
+
+    private func _updateContactSection() {
+        guard let section = self.dataSource.sectionWithKey("contact") else {
+            return
+        }
+
+        // Remove sections
+
+        guard let preferences = self.preferences, let contact = preferences.contact else {
+
+            if section.rowWithKey("select-contact") == nil {
+                let row = JSMStaticRow(key: "select-contact")
+                row.text = "Select contact…"
+                row.configurationForCell {
+                    (row, cell) in
+                    cell.textLabel?.textColor = UIColor(red: 0, green: 0.506, blue: 0.83, alpha: 1)
+                }
+                section.addRow(row)
+            }
+
+            if let recipients = self.dataSource.sectionWithKey("recipients") {
+                self.dataSource.removeSection(recipients)
+            }
+
+            return
+        }
+
+        // Contact Row
+
+        let contactRow: JSMStaticRow
+        if section.rowWithKey("contact") != nil {
+            contactRow = section.rowWithKey("contact")
+        }
+        else {
+            contactRow = JSMStaticRow(key: "contact")
+            section.addRow(contactRow)
+        }
+
+        if let imageData = contact.thumbnailImageData, let image = UIImage(data: imageData) {
+            let source = image.CGImage
+            let scale = UIScreen.mainScreen().scale
+
+            let size = 44 * scale
+
+            let context = CGBitmapContextCreate(nil, Int(size), Int(size), CGImageGetBitsPerComponent(source), 0, CGImageGetColorSpace(source), CGImageGetBitmapInfo(source).rawValue)
+
+            let percent = size / min(image.size.width * image.scale, image.size.height * image.scale)
+            let s = CGSize(width: image.size.width * image.scale * percent, height: image.size.height * image.scale * percent)
+            let o = CGPoint(x: ((s.width - size) / 2), y: ((s.height - size) / 2))
+            CGContextDrawImage(context, CGRect(origin: o, size: s), source)
+
+            if let imageRef = CGBitmapContextCreateImage(context) {
+                contactRow.image = UIImage(CGImage: imageRef, scale: scale, orientation: image.imageOrientation)
+            }
+        }
+
+        let formatter = CNContactFormatter()
+        contactRow.text = formatter.stringFromContact(contact)
+        contactRow.configurationForCell {
+            (row, cell) in
+            if let imageView = cell.imageView, let image = row.image {
+                imageView.layer.cornerRadius = image.size.width / 2
+                imageView.layer.masksToBounds = true
+            }
+        }
+    }
+
+    private func _updateRecipientSection() {
+        guard let preferences = self.preferences, let _ = preferences.contact else {
+
+            if let recipients = self.dataSource.sectionWithKey("recipients") {
+                self.dataSource.removeSection(recipients)
+            }
+
+            return
+        }
+
+        let section: JSMStaticSection
+        if self.dataSource.sectionWithKey("recipients") != nil {
+            section = self.dataSource.sectionWithKey("recipients")
+        }
+        else {
+            section = JSMStaticSection(key: "recipients")
+            self.dataSource.insertSection(section, atIndex: 1)
+        }
+
+        // Call Recipient
+
+        let callPreference: JSMStaticSelectPreference
+        if section.rowWithKey("call-recipient") as? JSMStaticSelectPreference != nil {
+            callPreference = section.rowWithKey("call-recipient") as! JSMStaticSelectPreference
+        }
+        else {
+            callPreference = JSMStaticSelectPreference.transientPreferenceWithKey("call-recipient")
+            callPreference.addObserver(self)
+            section.addRow(callPreference)
+        }
+
+        callPreference.text = "Calls"
+        callPreference.value = preferences.callRecipient
+        callPreference.options = preferences.callOptions.map({
+            (labelledValue) in
+            if let phoneNumber = labelledValue.value as? CNPhoneNumber {
+                return [JSMStaticSelectOptionLabel: phoneNumber.stringValue, JSMStaticSelectOptionValue: phoneNumber.stringValue]
+            }
+            return [JSMStaticSelectOptionLabel: labelledValue.value, JSMStaticSelectOptionValue: labelledValue.value]
+        })
+
+        // Message Recipient
+
+        let messagePreference: JSMStaticSelectPreference
+        if section.rowWithKey("message-recipient") as? JSMStaticSelectPreference != nil {
+            messagePreference = section.rowWithKey("message-recipient") as! JSMStaticSelectPreference
+        }
+        else {
+            messagePreference = JSMStaticSelectPreference.transientPreferenceWithKey("message-recipient")
+            messagePreference.addObserver(self)
+            section.addRow(messagePreference)
+        }
+
+        messagePreference.text = "Messages"
+        messagePreference.value = preferences.messageRecipient
+        messagePreference.options = preferences.messageOptions.map({
+            (labelledValue) in
+            if let phoneNumber = labelledValue.value as? CNPhoneNumber {
+                return [JSMStaticSelectOptionLabel: phoneNumber.stringValue, JSMStaticSelectOptionValue: phoneNumber.stringValue]
+            }
+            return [JSMStaticSelectOptionLabel: labelledValue.value, JSMStaticSelectOptionValue: labelledValue.value]
+        })
+    }
+
+    private func _updateMessagesSection() {
+        guard let section = self.dataSource.sectionWithKey("messages") else {
+            return
+        }
+
+        section.removeAllRows()
+
+        if let messages = self.preferences?.messages {
+            for message in messages {
+                let row = JSMStaticTextPreference.transientPreferenceWithKey(message)
+                row.value = message
+                row.textField?.placeholder = "Message Text"
+                row.textField?.returnKeyType = .Done
+                row.canBeMoved = true
+                row.canBeDeleted = true
+                row.fitControlToCell = true
+                row.addObserver(self)
+                section.addRow(row)
+            }
+        }
+
+        let row = JSMStaticRow(key: "add-message")
+        row.text = "Add Message"
+        row.canBeDeleted = true
+        row.configurationForCell {
+            (row, cell) in
+            cell.textLabel?.textColor = UIColor(red: 0, green: 0.506, blue: 0.83, alpha: 1)
+        }
+        section.addRow(row)
+    }
+
 	private func _updateMessagesInUserDefaults() {
 		if let preferences = self.preferences, let rows = self.dataSource?.sectionWithKey("messages")?.rows {
-			let filteredRows = rows.filter() {
-				($0 as? JSMStaticTextPreference)?.value != nil
-			} as! [JSMStaticTextPreference]
-			let messages = filteredRows.map() {
-				$0.value
-			} as! [String]
-			preferences.messages = messages
-		}
 
+            let values: [String?] = rows.map({ (row) in
+                guard let preference = row as? JSMStaticTextPreference else {
+                    return nil
+                }
+
+                guard let value = preference.value where value.characters.count > 0 else {
+                    return nil
+                }
+
+                return preference.value
+            })
+
+            preferences.messages = values.flatMap({ $0 })
+
+        }
 		else {
 			self._showMessage("There was a problem with saving your messages. Maybe you can give it another shot?")
 		}
