074b9d2df4f4578f733d3bc7c397a30ab1f6a5af
Daniel Farrelly <daniel@jellystyle.com>
2017-02-09 19:29:25 +1100

    Tweaking the share controller’s logic.

--- Send to Other/ShareViewController.swift
+++ Send to Other/ShareViewController.swift
@@ -1,3 +1,4 @@
+import Foundation
 import UIKit
 import MessageUI
 import MobileCoreServices
@@ -7,120 +8,208 @@ class ShareViewController: UIViewController, MFMessageComposeViewControllerDeleg
 	//! The messages view controller used to present the shared items.
 	var messageController: MFMessageComposeViewController?
 
-	// MARK: Handling extension requests
+	//! Valid UTIs in the preferred order of support.
+	let typeIdentifiers = [
+		kUTTypeFileURL,
+		kUTTypeURL,
+		kUTTypeJPEG,
+		kUTTypeJPEG2000,
+		kUTTypeTIFF,
+		kUTTypePICT,
+		kUTTypeGIF,
+		kUTTypePNG,
+		kUTTypeQuickTimeImage,
+		kUTTypeAppleICNS,
+		kUTTypeBMP,
+		kUTTypeICO,
+		kUTTypeImage,
+		kUTTypeQuickTimeMovie,
+		kUTTypeMPEG,
+		kUTTypeMPEG4,
+		kUTTypeMP3,
+		kUTTypeMPEG4Audio,
+		kUTTypeAppleProtectedMPEG4Audio,
+		kUTTypeAudiovisualContent,
+		kUTTypeText,
+		kUTTypePDF,
+		kUTTypeRTFD,
+		kUTTypeVCard
+	]
 
-	override func beginRequestWithExtensionContext(context: NSExtensionContext) {
-		super.beginRequestWithExtensionContext(context)
-
-		guard let preferences = PreferencesManager.sharedManager else {
-			return
-		}
-
-		guard let messageRecipient = preferences.messageRecipient where messageRecipient.characters.count > 0 else {
-			return
-		}
-
-		let messageController = MFMessageComposeViewController()
-		messageController.messageComposeDelegate = self
-		messageController.recipients = [messageRecipient]
-
-		let typeIdentifiers = [kUTTypeFileURL, kUTTypeURL, kUTTypeJPEG, kUTTypeJPEG2000, kUTTypeTIFF, kUTTypePICT, kUTTypeGIF, kUTTypePNG, kUTTypeQuickTimeImage, kUTTypeAppleICNS, kUTTypeBMP, kUTTypeICO, kUTTypeImage, kUTTypeQuickTimeMovie, kUTTypeMPEG, kUTTypeMPEG4, kUTTypeMP3, kUTTypeMPEG4Audio, kUTTypeAppleProtectedMPEG4Audio, kUTTypeAudiovisualContent, kUTTypeText, kUTTypePDF, kUTTypeRTFD, kUTTypeVCard]
-
-        var items = 0
-		for item in context.inputItems as? [NSExtensionItem] ?? [] {
-			for itemProvider in item.attachments as? [NSItemProvider] ?? [] {
-				for typeIdentifier in typeIdentifiers where self.attemptToHandle(itemProvider, typeIdentifier: typeIdentifier) {
-                    items += 1
-
-                    break
-				}
-			}
-		}
-
-		if items == 0 {
-			return
-		}
-
-		self.messageController = messageController
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+		self.initialize()
 	}
 
-	func attemptToHandle(itemProvider: NSItemProvider!, typeIdentifier: CFString!) -> Bool {
-		if !itemProvider.hasItemConformingToTypeIdentifier(typeIdentifier as String) {
-			return false
-		}
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		self.initialize()
+	}
 
-		itemProvider.loadItemForTypeIdentifier(typeIdentifier as String, options: nil, completionHandler: { item, error in
-			guard let messageController = self.messageController else {
-				return
-			}
-            
-			if let url = item as? NSURL where url.fileURL, let data = NSData(contentsOfURL: url), let filename = url.lastPathComponent {
-                messageController.addAttachmentData(data, typeIdentifier: typeIdentifier as String, filename: filename)
-            }
-            else if let url = item as? NSURL where url.fileURL {
-                messageController.addAttachmentURL(url, withAlternateFilename: nil)
-            }
-            else if let url = item as? NSURL, let text = url.absoluteString {
-                messageController.body = "\(messageController.body ?? "") \(text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()))"
-            }
-			else if let text = item as? String {
-                messageController.body = "\(messageController.body ?? "") \(text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()))"
-			}
-			else if let data = item as? NSData, let ext = UTTypeCopyPreferredTagWithClass(typeIdentifier, kUTTagClassFilenameExtension)?.takeRetainedValue() {
-				messageController.addAttachmentData(data, typeIdentifier: typeIdentifier as String, filename: "attachment.\(ext)")
-			}
-		})
-
-		return true
+	private func initialize() {
+		self.modalPresentationStyle = .OverFullScreen
 	}
 
 	// MARK: View life cycle
 
-	override func viewWillAppear(animated: Bool) {
-		super.viewWillAppear(animated)
+	var activityIndicator: UIActivityIndicatorView!
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		self.view.backgroundColor = UIColor.clearColor()
+	}
+
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+
+		self.handleExtensionRequest()
+	}
+
+	private var handling = false
+	
+	private func handleExtensionRequest() {
 		guard let context = self.extensionContext else {
 			return
 		}
 
-		// We have to handle our "error" states here so we have a view to show our alerts on.
+		dispatch_async(dispatch_get_main_queue()) {
+			guard self.handling == false else {
+				return
+			}
 
-		guard let preferences = PreferencesManager.sharedManager else {
-			let message = "Something went wrong while loading your preferences. Have another go in a minute or two."
-			let alert = UIAlertController.alert(message, handler: { action in
-				context.completeRequestReturningItems([], completionHandler: nil)
-			})
-			self.presentViewController(alert, animated: true, completion: nil)
-			return
+			self.handling = true
+
+			guard let preferences = PreferencesManager.sharedManager else {
+				let message = "Something went wrong while loading your preferences. Have another go in a minute or two."
+				let alert = UIAlertController.alert(message, handler: { action in
+					context.completeRequestReturningItems([], completionHandler: nil)
+				})
+				self.presentViewController(alert, animated: true, completion: nil)
+				return
+			}
+
+			guard let messageRecipient = preferences.messageRecipient where messageRecipient.characters.count > 0 else {
+				let message = "There's no recipient for messages selected in your preferences. You need to set it up in the app before using this extension."
+				let alert = UIAlertController.alert(message, handler: { action in
+					context.completeRequestReturningItems([], completionHandler: nil)
+				})
+				self.presentViewController(alert, animated: true, completion: nil)
+				return
+			}
+
+			let messageController = MFMessageComposeViewController()
+			messageController.messageComposeDelegate = self
+			messageController.recipients = [messageRecipient]
+
+			dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+				var numberOfAttachments = 0
+
+				print("[ShareViewController] Number of input items: \(context.inputItems.count)")
+
+				for item in context.inputItems as? [NSExtensionItem] ?? [] {
+					guard let attachments = item.attachments as? [NSItemProvider] else {
+						continue
+					}
+
+					for itemProvider in attachments {
+						print("[ShareViewController] Attempting to attach \(itemProvider)…")
+
+						let beforeAttachment = numberOfAttachments
+
+						for typeIdentifier in self.typeIdentifiers {
+							if !itemProvider.hasItemConformingToTypeIdentifier(typeIdentifier as String) {
+								continue
+							}
+
+							print("[ShareViewController] Loading item for `\(typeIdentifier)`…")
+
+							var finished = false
+
+							itemProvider.loadItemForTypeIdentifier(typeIdentifier as String, options: nil, completionHandler: { item, error in
+								print("[ShareViewController] Load complete.")
+
+								if item == nil {
+									print("[ShareViewController] Nothing to attach?")
+								}
+								else if let url = item as? NSURL where url.fileURL, let filename = url.lastPathComponent {
+
+									// To bypass an issue with MFMessageComposeViewController where photos and videos attached using file URLs
+									// are displayed as a black preview (even though they send fine), we load and attach the data instead. This
+									// works fine unless the data causes the memory constraints for the extension (causing it to crash), so we
+									// only do so for photos, and just accept the downside for videos for now.
+
+									if UTTypeConformsTo(typeIdentifier, kUTTypeImage), let data = NSData(contentsOfURL: url) {
+										print("[ShareViewController] Attaching as file data…")
+										messageController.addAttachmentData(data, typeIdentifier: typeIdentifier as String, filename: filename)
+									}
+									else {
+										print("[ShareViewController] Attaching as url…")
+										messageController.addAttachmentURL(url, withAlternateFilename: filename)
+									}
+								}
+								else if let url = item as? NSURL, let text = url.absoluteString {
+									print("[ShareViewController] Attaching as text…")
+									messageController.body = "\(messageController.body ?? "") \(text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()))"
+								}
+								else if let text = item as? String {
+									print("[ShareViewController] Attaching as text…")
+									messageController.body = "\(messageController.body ?? "") \(text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()))"
+								}
+								else if let data = item as? NSData, let ext = UTTypeCopyPreferredTagWithClass(typeIdentifier, kUTTagClassFilenameExtension)?.takeRetainedValue() {
+									print("[ShareViewController] Attaching as data…")
+									messageController.addAttachmentData(data, typeIdentifier: typeIdentifier as String, filename: "attachment.\(ext)")
+								}
+								else if let item = item {
+									print("[ShareViewController] Unknown item provided: \(item)")
+								}
+
+								finished = true
+							})
+
+							while !finished {
+								usleep(5000)
+							}
+
+							numberOfAttachments += 1
+
+							break
+						}
+
+						if beforeAttachment == numberOfAttachments {
+							print("[ShareViewController] Couldn't attach.")
+						}
+						else {
+							print("[ShareViewController] Success!")
+						}
+					}
+				}
+
+				dispatch_async(dispatch_get_main_queue()) {
+					guard numberOfAttachments > 0 else {
+						let message = "Either no items were available to share, or they're not supported. Sorry!"
+						let alert = UIAlertController.alert(message, handler: { action in
+							context.completeRequestReturningItems([], completionHandler: nil)
+						})
+
+						self.presentViewController(alert, animated: true, completion: nil)
+
+						return
+					}
+
+					self.presentViewController(messageController, animated: true, completion: nil)
+				}
+			}
 		}
-
-		guard let messageRecipient = preferences.messageRecipient where messageRecipient.characters.count > 0 else {
-			let message = "There's no recipient for messages selected in your preferences. You need to set it up in the app before using this extension."
-			let alert = UIAlertController.alert(message, handler: { action in
-				context.completeRequestReturningItems([], completionHandler: nil)
-			})
-			self.presentViewController(alert, animated: true, completion: nil)
-			return
-		}
-
-		guard let messageController = self.messageController else {
-			let message = "Either no items were available to share, or they're not supported. Sorry!"
-			let alert = UIAlertController.alert(message, handler: { action in
-				context.completeRequestReturningItems([], completionHandler: nil)
-			})
-			self.presentViewController(alert, animated: true, completion: nil)
-			return
-		}
-
-		// If we got to here, we can go ahead and present the message controller
-
-		self.presentViewController(messageController, animated: true, completion: nil)
 	}
 
 	// MARK: Message compose view delegate
 
 	func messageComposeViewController(controller: MFMessageComposeViewController, didFinishWithResult result: MessageComposeResult) {
+		print("[ShareViewController] Did finish messaging…")
+
 		PreferencesManager.sharedManager?.didFinishMessaging(result)
+
 		controller.dismissViewControllerAnimated(true) {
 			if let extensionContext = self.extensionContext {
 				extensionContext.completeRequestReturningItems([], completionHandler: nil)
